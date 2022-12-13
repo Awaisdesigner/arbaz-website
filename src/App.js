54d07c927eedf69225c5822b92aa271d860a3923
Awaisdesigner <awaisdesigner50@gmail.com>
2022-12-13 23:03:28 +0500

    arbaz

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ConfigRoute from "./routing/ConfigRoute";
+
+
+const App = () => {
+  return (
+    <>
+      <ConfigRoute />
+    </> 
+  );
+};
+
+export default App;
+
+
+
